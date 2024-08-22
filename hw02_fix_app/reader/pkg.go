2efcf1c15a04c4f29385b28cfeb4ad4dfbcf3890
Victoria-290 <2419065@mail.ru>
2024-08-22 23:11:04 +0300

    Исправление ошибок линтера

--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -12,20 +12,20 @@ import (
 func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open file: %v", err)
+		return nil, fmt.Errorf("failed to open file: %w", err)
 	}
 	defer f.Close() // Закрыла файл после завершения функции
 
 	fileContent, err := io.ReadAll(f)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read file: %v", err)
+		return nil, fmt.Errorf("failed to read file: %w", err)
 	}
 
 	var data []types.Employee
 
 	err = json.Unmarshal(fileContent, &data)
 	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
+		return nil, fmt.Errorf("failed to unmarshal JSON: %w", err)
 	}
 
 	if limit > 0 && limit < len(data) {
